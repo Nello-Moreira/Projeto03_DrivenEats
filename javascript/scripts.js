bd3c10d8e5aa987f1f547fb31624ff037a014693
Gabriel Moreira <g.moreira1@outlook.com>
2021-07-23 15:53:44 -0300

    feature implemented: confirmation button and whatsapp message

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -10,38 +10,44 @@ function changeActivationStatus(){
     const parent_id = this.parentNode.parentNode.getAttribute("id");
     const parent_section = document.getElementById(parent_id);
     const options = parent_section.getElementsByTagName("li");
-    let active_string = " active";
+    let active_class = "active";
+    let hidden_class = "hidden";
     
-    if (this.className.indexOf(active_string) !== -1){
-        optionDeactivation(this, active_string, parent_id);
+    if (this.className.indexOf(active_class) !== -1){
+        optionDeactivation(this, active_class, hidden_class, parent_id);
     }else{
-        let activated = indexOfActivetedOption(options, active_string);
-        if (activated !== -1){
-            optionDeactivation(options[activated], active_string, parent_id);
+        let active_index = indexOfActiveOption(options, active_class);
+        if (active_index !== -1){
+            optionDeactivation(options[active_index], active_class, hidden_class, parent_id);
         }
-        optionActivation(this, active_string, parent_id);
+        optionActivation(this, active_class, hidden_class, parent_id);
     }
+
+    confirmationButtonChanger();
 }
 
-function optionActivation(option, active_string, section){
-    option.className = option.className + active_string;
-    option.getElementsByClassName("icon")[0].style.display = "initial";
+function optionActivation(option, active_class, hidden_class, section){
+    // Border activation
+    option.classList.add(active_class);
+    // Icon activation
+    option.getElementsByClassName("icon")[0].classList.remove(hidden_class);
     let option_name = option.getElementsByClassName("option-name")[0].innerHTML
     let price = getPrice(option.getElementsByClassName("price")[0].innerHTML);
     bill[section] = [option_name, price];
 }
 
-function optionDeactivation(option, active_string, section){
-    let stop_index = option.className.indexOf(active_string)
-    option.className = option.className.slice(0, stop_index);
-    option.getElementsByClassName("icon")[0].style.display = "none";
+function optionDeactivation(option, active_class, hidden_class, section){
+    // Border deactivation
+    option.classList.remove(active_class);
+    // Icon deactivation
+    option.getElementsByClassName("icon")[0].classList.add(hidden_class);
     bill[section] = [];
 }
 
-function indexOfActiveOption(options, active_string){
+function indexOfActiveOption(options, active_class){
     let index = -1;
     for (let i = 0; i < options.length; i++){
-        if (options[i].className.indexOf(active_string) !== -1){
+        if (options[i].classList.contains(active_class)){
             return i;
         }
     }
@@ -55,12 +61,101 @@ function getPrice(price_string){
     return Number(price_string)
 }
 
+function confirmationButtonChanger(){
+    button_div = document.getElementsByClassName("bottom-button")[0];
+    confirmation_button = button_div.getElementsByTagName("input")[0];
+
+    if (bill.meal.length > 0 && bill.drink.length > 0 && bill.dessert.length > 0){
+        confirmation_button.className = "button-enabled";
+        confirmation_button.value = "Fechar pedido";
+    }else{
+        confirmation_button.className = "button-disabled";
+        confirmation_button.value = "Selecione os 3 itens para fechar o pedido";
+    }
+}
+
+function openBillWindow(){
+    // Infos refresh
+    getBillCategoryInfos("meal");
+    getBillCategoryInfos("drink");
+    getBillCategoryInfos("dessert");   
+    document.getElementById("total-cost").innerHTML = brRealFormatter(bill.total_charge());
+
+    // Bill display
+    document.getElementsByClassName("bill-background")[0].classList.remove("hidden");
+
+}
+
+function closeBillWindow(){
+    document.getElementsByClassName("bill-background")[0].classList.add("hidden");
+}
+
+function getBillCategoryInfos(category){
+    document.getElementById(category + "-name").innerHTML = bill[category][0];
+    document.getElementById(category + "-price").innerHTML = moneyFormatter(bill[category][1]);
+}
+
+function moneyFormatter(number){
+    number = number.toFixed(2);
+    let money_string = number.toString();
+    money_string = money_string.replace(".", ",");
+    return money_string;
+}
+
+function brRealFormatter(number){
+    let real =   moneyFormatter(number);
+    return "R$ " + real;
+}
+
+function nameRequest(){
+    return prompt("Por favor, digite seu nome");
+}
+
+function adressRequest(){
+    return prompt("Digite seu endereço");
+}
+
+function sendWhatsAppMessage(){
+    const persons_name = nameRequest();
+    const adress = adressRequest();
+    const meal_name = bill.meal[0];
+    const drink_name = bill.drink[0];
+    const dessert_name = bill.dessert[0];
+    const total_cost = brRealFormatter(bill.total_charge());
+
+    let message = `Olá, gostaria de fazer o pedido:\n`
+    + `- Prato: ${meal_name}\n`
+    + `- Bebida: ${drink_name}\n`
+    + `- Sobremesa: ${dessert_name}\n`
+    + `Total: ${total_cost}\n\n`
+    + `Nome: ${persons_name}\n`
+    + `Endereço: ${adress}`;
+
+    let link = "https://wa.me/5521999812307?text=" + encodeURIComponent(message);
+
+    window.open(link, "_blank");
+    window.location.reload();
+}
+
 const bill = {
     meal: [], 
     drink: [], 
     dessert: [], 
-    status: function (){
-        return "meal: " + this.meal + ", drink: " + this.drink + ", dessert: " + this.dessert
+    total_charge: function(){
+        let sum = 0;
+
+        if (this.meal.length > 0){
+            sum += this.meal[1];
+        }
+
+        if (this.drink.length > 0){
+            sum += this.drink[1];
+        }
+
+        if (this.dessert.length > 0){
+            sum += this.dessert[1];
+        }
+        return sum;
     }
 };
 
